894727b12b6b677ab307e0cc155cf07ae09870e4
StevenMODIMO <stevepetes26@gmail.com>
2023-11-30 19:00:45 +0300

    Project Setup and Initialization

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6UypRfn0v0IRdoHWlr5JIP4rvvmGVedk",
+  authDomain: "react-firebase-18a3c.firebaseapp.com",
+  projectId: "react-firebase-18a3c",
+  storageBucket: "react-firebase-18a3c.appspot.com",
+  messagingSenderId: "664523658595",
+  appId: "1:664523658595:web:f262bff473600a0c39ef56",
+  measurementId: "G-4N9993F9HY"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = ''
