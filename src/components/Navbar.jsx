88fdacfd5069b2ae81e46986f193b9d301bb2928
StevenMODIMO <stevepetes26@gmail.com>
2023-12-01 15:06:37 +0300

    added: React Icon in the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='bg-gray-700/10 p-4 text-white'>
-        <header className='text-2xl flex gap-2'>
-            <img src='/1611674.png' alt='logo' className='w-10' />
-            <h1>Task Manager</h1>
-        </header>
+    <div className="bg-gray-700/10 p-4 text-white">
+      <header className="text-2xl flex gap-2">
+        <img src="/1611674.png" alt="logo" className="w-10" />
+        <h1>Task Manager</h1>
+        <img src="/react.svg" alt="logo" className="w-5" />
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
